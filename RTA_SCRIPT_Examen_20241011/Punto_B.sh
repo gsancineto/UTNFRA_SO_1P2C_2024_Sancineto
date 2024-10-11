969deb11a2ec4d818e0ab74f175e6dab6cc310b0
gsancineto <jgabrielsancineto@gmail.com>
2024-10-11 18:53:00 +0000

    add: punto_b terminado

--- RTA_SCRIPT_Examen_20241011/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_B.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#DISCO_2GB = $(sudo fdisk -l | grep '2 GiB' | awk '{print $2}' | awk -F ':' '{print $1}')
-#DISCO_10GB = $(sudo fdisk -l | grep '10 GiB' | awk '{print $2}' | awk -F ':' '{print $1}')
+DISCO_2GB= $(sudo fdisk -l | grep '2 GiB' | awk '{print $2}' | awk -F ':' '{print $1}')
+DISCO_10GB= $(sudo fdisk -l | grep '10 GiB' | awk '{print $2}' | awk -F ':' '{print $1}')
 
 #Agregar disco 2gb
-#cambiar /dev/sdd por variable DISCO_2GB
-sudo fdisk /dev/sdd << EOF
+sudo fdisk $DISCO_2GB << EOF
 n
 p
 1
@@ -15,8 +14,7 @@ w
 EOF
 
 #Agregar disco 10gb
-#cambiar /dev/sdc por variable DISCO_10GB
-sudo fdisk /dev/sdc << EOF 
+sudo fdisk $DISCO_10GB << EOF 
 n
 e
 1
@@ -37,5 +35,19 @@ EOF
 
 #Formateo las 10 particiones como ext4
 for i in {5..14}; do
-	sudo mkfs -t ext4 /dev/sdc$i
+	sudo mkfs -t ext4 $DISCO_10GB$i
 done
+
+#Monto con persistencia
+alumno=1
+parcial=1
+for i in {5..13}; do
+	echo "$DISCO_10GB$i /Examenes-UTN/alumno_$alumno/parcial_$parcial ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	a=$((a + 1))
+	if [ $a -eq 4 ]; then
+		p=$((p + 1))
+		a=1
+	fi
+done
+echo "${DISCO_10GB}14 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo mount -a
